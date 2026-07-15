a2c920fb6119cdc266e9661e061ea75ae9d93fd7
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2026-07-15 20:11:25 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        s = s.toLowerCase();
+
+        /**
+         * Extra Space - Time - O(n); Space - O(n)
+         
+            let filteredString = '';
+            let rev = '';
+
+            for (let i = 0; i < s.length; i++) {
+                if (s[i].match(/[a-z0-9]/i)) {
+                    filteredString = filteredString + s[i]
+                    rev = s[i] + rev
+                }
+            }
+            return filteredString === rev;
+        */
+
+        /** Optimised => Two Pointer Approach
+         * Time - O(n); Space - O(1)
+         */
+
+        let i = 0;
+        let j = s.length - 1;
+        while (i < j) {
+            if (!s[i].match(/[a-z0-9]/i)) {
+                i++;
+            } else if (!s[j].match(/[a-z0-9]/i)) {
+                j--;
+            } else if (s[i] === s[j]) {
+                i++;
+                j--;
+            } else return false;
+        }
+        return true;
+    }
+}
