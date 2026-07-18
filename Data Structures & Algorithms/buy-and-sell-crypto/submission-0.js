690b5970b7d35b07a841c924b9b517742ed80a6f
Sanjay Kumar S R <49760746+sanju1098@users.noreply.github.com>
2026-07-18 19:21:48 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let min = prices[0];
+        let max = 0;
+        for (let i = 1; i < prices.length; i++) {
+            if (prices[i] - min > max) {
+                max = prices[i] - min;
+            }
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+        }
+        return max;
+    }
+}
